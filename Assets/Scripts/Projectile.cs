65d8f581bfb2f61d92ff43ac26214f1aff26e980
Marc Soliman <71621832+MarcSoliman@users.noreply.github.com>
2022-09-04 21:32:43 -0500

    Created Player Projectile
    Projectile feedback still in progress

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float _forceFactor = .10f;
+
+    [SerializeField] private float _damage = 50;
+
+    [SerializeField] private ParticleSystem _particleSystem;
+
+    [SerializeField] private AudioClip _shootAudio;
+    [SerializeField] private AudioClip _impactAudio;
+    protected GameObject _parentGameObj;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        gameObject.SetActive(false);
+    }
+}
