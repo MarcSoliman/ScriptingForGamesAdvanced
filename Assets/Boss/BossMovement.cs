cad3f4e9d003e40567065d6431dd3372de12f9f6
Marc Soliman <71621832+MarcSoliman@users.noreply.github.com>
2022-09-13 17:04:10 -0500

    Using NavMeshPath for custom movement implementation for RigidBody Boss

--- Assets/Boss/BossMovement.cs
+++ Assets/Boss/BossMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,52 +6,136 @@ using UnityEngine.AI;
 
 public class BossMovement : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] private PlayerDetectionRange _playerDetection;
+    [SerializeField] private LookAtTarget _lookAtTarget;
     [SerializeField] private GameObject _player;
+    [SerializeField] private NavMeshPath _path;
 
-    private NavMeshAgent _navMeshAgent;
 
+
+    [Header("Movement Variables")]
+    [SerializeField] private float _flyHeight = 5f;
+    [SerializeField] private float _flySpeed = 4f;
+    [SerializeField] private float _fleeSpeed = 2f;
+    [SerializeField] private float _followSpeed = 6f;
+    private Rigidbody _RB;
+    private float elapsed;
     private Transform _targetPosition;
+
+    private bool _isFlying = false;
+    private bool _isLanding = false;
+
+    private Vector3 _landingPosition;
+
+    private Vector3[] _pathPoints = new Vector3[] { Vector3.zero, Vector3.zero, Vector3.zero };
+
     private void Awake()
     {
-        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _RB = GetComponent<Rigidbody>();
+
     }
     // Start is called before the first frame update
     void Start()
     {
-
+        _path = new NavMeshPath();
+        elapsed = 0.0f;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        FollowPlayer();
+
+
+        FleePlayer();
+        //FlyAttackMovement();
     }
 
     void FollowPlayer()
     {
 
-        if (_playerDetection.PlayerDetected)
-        {
-            //move towards player
-            _targetPosition = _player.transform;
-            _navMeshAgent.destination = _player.transform.position;
-            print("following");
+        if (!_playerDetection.PlayerDetected) return;
 
+        _lookAtTarget.LookAtPlayer(1);
+        _targetPosition = _player.transform;
+
+        elapsed += Time.deltaTime;
+
+        if (elapsed > 1.0f)
+        {
+            NavMesh.CalculatePath(transform.position, _targetPosition.position, NavMesh.AllAreas, _path);
+
+            _path.GetCornersNonAlloc(_pathPoints);
+            print(_pathPoints.Length);
         }
+
+
+        //move to final point of the navmesh path
+        for (int i = 0; i < _pathPoints.Length - 1; i++)
+        {
+            _RB.MovePosition(Vector3.MoveTowards(transform.position, _pathPoints[i], _followSpeed * Time.deltaTime));
+
+            Debug.DrawLine(_path.corners[i], _path.corners[i + 1], Color.red);
+        }
+
+
+
 
     }
 
     void FleePlayer()
     {
+        _lookAtTarget.LookAtPlayer(.1f);
 
-        if (_playerDetection.PlayerDetected)
+        if (!_playerDetection.PlayerDetected) return;
+
+        var _fleePos = (transform.position - _player.transform.position).normalized * 10f;
+
+        elapsed += Time.deltaTime;
+
+
+
+        if (elapsed > 1.0f)
         {
-            //move away from player
-            _targetPosition = _player.transform;
-            _navMeshAgent.destination = -_player.transform.position;
+            NavMesh.CalculatePath(transform.position, _fleePos, NavMesh.AllAreas, _path);
 
+            _path.GetCornersNonAlloc(_pathPoints);
+            print(_pathPoints.Length);
         }
+
+        //move to final point of the navmesh path
+        for (int i = 0; i < _pathPoints.Length - 1; i++)
+        {
+            _RB.MovePosition(Vector3.MoveTowards(transform.position, _pathPoints[i], _followSpeed * Time.deltaTime));
+
+            //Debug.DrawLine(_path.corners[i], _path.corners[i + 1], Color.red);
+        }
+
+    }
+
+    void FlyAttackMovement()
+    {
+
+        if (_isLanding) return;
+
+        _isFlying = true;
+        _lookAtTarget.LookDown(1f);
+
+
+        //call Landing() after 5 seconds
+        Invoke(nameof(LandingBegin), 5f);
+    }
+
+    private void LandingBegin()
+    {
+        _isLanding = true;
+        _lookAtTarget.LookAtPlayer(.2f);
+
+        if (transform.position.y <= 0.5f)
+        {
+            _isFlying = false;
+        }
+
 
     }
 }
--- Assets/Boss/LookAtTarget.cs
+++ Assets/Boss/LookAtTarget.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class LookAtTarget : MonoBehaviour
 {
     [SerializeField] private Transform _lookAtTarget;
-    void Start()
+
+
+
+    public void LookAtPlayer(float lookSpeed)
     {
-        
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_lookAtTarget.position - transform.position), lookSpeed * Time.deltaTime);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LookDown(float lookSpeed)
     {
-        transform.LookAt(_lookAtTarget,Vector3.up);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.down), lookSpeed * Time.deltaTime);
     }
+
+    public void LookAway(float lookSpeed)
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(transform.position - _lookAtTarget.position), lookSpeed * Time.deltaTime);
+    }
+
+
+
 }
